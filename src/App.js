3504fb9ec2e9290134d53d666debcf60214660a9
abellasjuan <juanvictoraa@gmail.com>
2022-05-17 21:14:02 -0300

    feat: create context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
 
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import { AuthProvider } from './contexts/authContext.js';
+
+function App(){
   return (
-    <p>TEST</p>
-  );
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/' element={<SignIn/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
 }
 
 export default App;
