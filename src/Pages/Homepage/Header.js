7931b19e79a2c18c6a5be9743873cff16e645ac8
abellasjuan <juanvictoraa@gmail.com>
2022-05-21 13:15:48 -0300

    feat: create user context

--- src/Pages/Homepage/Header.js
+++ src/Pages/Homepage/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import '../../styles/header.css';
+import useUserInfo from '../../hooks/useUserInfo';
 
 export default function Header({black}){
+
+    const { user } = useUserInfo();
+
     return(
         <div className={black ? "header-top black" : "header-top"}>
             <strong>CINE.ME</strong> 
-            
             <div className="header-user">
-                <span>USERNAME</span>
+                <span>{user.userName}</span>
                     <a href="/">
                         <img src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png" alt="Usuario" />
                     </a>
             </div>
-            
         </div>
     );
 }
